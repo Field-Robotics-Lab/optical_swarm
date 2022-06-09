f38374e9bc4ada4d8344a44eadb9f2f675bd23ca
matthewheubach <mattheubach@live.com>
2022-06-09 14:31:12 -0700

    add aprilTag_thrustcmd

---
+++ matlab/aprilTag_thrustcmd.m
@@ -0,0 +1,21 @@
+function [left,right] = aprilTag_thrustcmd(trans)
+% aprilTag2cmd_vel receives tf translation data from the apriltag_ros
+% wrapper and calculates left and right thrust commands
+
+X = trans.X;
+Y = trans.Y;
+Z = trans.Z;
+
+dist = norm([X Y Z]);
+% angle = quat2eul([w x y z]);
+% head = rad2deg(angle(1))
+psi=atan2d(Y,Z);
+
+% Linear and Angular gains and command velocities
+k_v=0.01;
+k_h=1;
+turn = k_h*psi;
+
+left = (k_v*dist)-turn;
+right = (k_v*dist)+turn;
+end
--- matlab/april_tracking.m
+++ matlab/april_tracking.m
@@ -18,18 +18,29 @@ tag_tf = rossubscriber('/tf','DataFormat','struct');
 
 
 % Setup Publisher
-cmd_pub = rospublisher('/sandwich_0/cmd_vel','geometry_msgs/Twist');
-cmd_msg = rosmessage(cmd_pub);
+% cmd_pub = rospublisher('/sandwich_0/cmd_vel','geometry_msgs/Twist');
+% cmd_msg = rosmessage(cmd_pub);
 
+left_pub = rospublisher('/sandwich_0/thrusters/left_thrust_cmd','std_msgs/Float32');
+right_pub = rospublisher('/sandwich_0/thrusters/right_thrust_cmd','std_msgs/Float32');
+left_msg = rosmessage(left_pub);
+right_msg = rosmessage(right_pub);
 
 while true
-tag = receive(tag_tf,3);
+tag = receive(tag_tf);
 trans = tag.Transforms.Transform.Translation;
 rot = tag.Transforms.Transform.Rotation;
-[v_c,r_c] = aprilTag2cmd_vel(trans);
-cmd_msg.Linear.X = v_c
-cmd_msg.Angular.Z = r_c
-send(cmd_pub, cmd_msg);
+[left,right] = aprilTag_thrustcmd(trans);
+left_msg.Data = left
+right_msg.Data = right
+send(left_pub, left_msg);
+send(right_pub, right_msg);
+
+% [v_c,r_c] = aprilTag2cmd_vel(trans);
+% cmd_msg.Linear.X = v_c
+% cmd_msg.Angular.Z = r_c
+% send(cmd_pub, cmd_msg);
+
 waitfor(rate);
 end
 
