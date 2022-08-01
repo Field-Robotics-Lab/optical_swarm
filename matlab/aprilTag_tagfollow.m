07db87714ef057b12fd4799bb462741b768344fd
matthewheubach <mattheubach@live.com>
2022-08-01 13:25:01 -0700

    Disappering model

--- matlab/aprilTag_tagfollow.m
+++ matlab/aprilTag_tagfollow.m
@@ -3,8 +3,9 @@ function [left,right] = aprilTag_tagfollow(ID,dist,head)
 % left and right turn commands to follow AprilCube rabbit model2
 
 
-% AprilCube Tag numbers are 1-4
-ind = find(ID >=5);
+% AprilCube Tag numbers are 90-95
+ind = find(ID >=90);
+debug = ID(ind)
 dist = dist(ind);
 head = head(ind);
 
@@ -30,8 +31,8 @@ elseif val <=30 && val>5
 else
     turn = 0;
 end
-k_v = 0.3;
-k_r = 1.5;
+k_v = 0.1; % default 0.3
+k_r = 1.0; % default 1.5
 left = k_v*fwd -k_r*(sign(psi)*turn);
 right = k_v*fwd + k_r*(sign(psi)*turn);
     
--- matlab/multi_follow.m
+++ matlab/multi_follow.m
@@ -12,7 +12,7 @@ end
 % ROS Calculation Rate
 desiredRate = 10; %10 hertz 
 rate = rateControl(desiredRate);
-
+tftree = rostf;
 % --------------- Tag Detection Subscribers -----------------------------
 
 %---------------------- Boat 0 ----------------------------
@@ -23,6 +23,13 @@ sand0_side_right_sub = rossubscriber('/robot0/sandwich_0/sensors/cameras/side_ri
 sand0_side_left_sub = rossubscriber('/robot0/sandwich_0/sensors/cameras/side_left_camera/tag_detections','apriltag_ros/AprilTagDetectionArray','DataFormat','struct');
 sand0_rear_right_sub = rossubscriber('/robot0/sandwich_0/sensors/cameras/rear_right_camera/tag_detections','apriltag_ros/AprilTagDetectionArray','DataFormat','struct');
 sand0_rear_left_sub = rossubscriber('/robot0/sandwich_0/sensors/cameras/rear_left_camera/tag_detections','apriltag_ros/AprilTagDetectionArray','DataFormat','struct');
+
+sand0_front_right_xform = getTransform(tftree,'sandwich_0/base_link','sandwich_0/front_right_camera_link_optical','Timeout',inf);  
+sand0_front_left_xform = getTransform(tftree,'sandwich_0/base_link','sandwich_0/front_left_camera_link_optical','Timeout',inf); 
+sand0_side_right_xform = getTransform(tftree,'sandwich_0/base_link','sandwich_0/side_right_camera_link_optical','Timeout',inf); 
+sand0_side_left_xform = getTransform(tftree,'sandwich_0/base_link','sandwich_0/side_left_camera_link_optical','Timeout',inf); 
+sand0_rear_right_xform = getTransform(tftree,'sandwich_0/base_link','sandwich_0/rear_right_camera_link_optical','Timeout',inf); 
+sand0_rear_left_xform = getTransform(tftree,'sandwich_0/base_link','sandwich_0/rear_left_camera_link_optical','Timeout',inf); 
 
 % -------------------- Boat 1 ---------------------------------------
 
@@ -43,9 +50,6 @@ sand1_rear_left_sub =   rossubscriber('/robot1/sandwich_1/sensors/cameras/rear_l
 % sand2_rear_left_sub =   rossubscriber('/robot2/sandwich_2/sensors/cameras/rear_left_camera/tag_detections','apriltag_ros/AprilTagDetectionArray','DataFormat','struct');
 % 
 
-
-
-
 % ---------- Thrust Command Publishers ----------------------------
 
 sandwich_0_left_pub = rospublisher('/sandwich_0/thrusters/left_thrust_cmd','std_msgs/Float32');
@@ -53,10 +57,10 @@ sandwich_0_right_pub = rospublisher('/sandwich_0/thrusters/right_thrust_cmd','st
 sandwich_0_left_msg = rosmessage(sandwich_0_left_pub);
 sandwich_0_right_msg = rosmessage(sandwich_0_right_pub);
 
-sandwich_1_left_pub = rospublisher('/sandwich_1/thrusters/left_thrust_cmd','std_msgs/Float32');
-sandwich_1_right_pub = rospublisher('/sandwich_1/thrusters/right_thrust_cmd','std_msgs/Float32');
-sandwich_1_left_msg = rosmessage(sandwich_1_left_pub);
-sandwich_1_right_msg = rosmessage(sandwich_1_right_pub);
+% sandwich_1_left_pub = rospublisher('/sandwich_1/thrusters/left_thrust_cmd','std_msgs/Float32');
+% sandwich_1_right_pub = rospublisher('/sandwich_1/thrusters/right_thrust_cmd','std_msgs/Float32');
+% sandwich_1_left_msg = rosmessage(sandwich_1_left_pub);
+% sandwich_1_right_msg = rosmessage(sandwich_1_right_pub);
 
 % sandwich_2_left_pub = rospublisher('/sandwich_2/thrusters/left_thrust_cmd','std_msgs/Float32');
 % sandwich_2_right_pub = rospublisher('/sandwich_2/thrusters/right_thrust_cmd','std_msgs/Float32');
@@ -64,18 +68,8 @@ sandwich_1_right_msg = rosmessage(sandwich_1_right_pub);
 % sandwich_2_right_msg = rosmessage(sandwich_2_right_pub);
 
 
-%%
-
 while true
-    tftree = rostf;
-
-% ----------------------- Boat 0 -------------------------
-sand0_front_right_xform = getTransform(tftree,'sandwich_0/base_link','sandwich_0/front_right_camera_link_optical','Timeout',inf);  
-sand0_front_left_xform = getTransform(tftree,'sandwich_0/base_link','sandwich_0/front_left_camera_link_optical','Timeout',inf); 
-sand0_side_right_xform = getTransform(tftree,'sandwich_0/base_link','sandwich_0/side_right_camera_link_optical','Timeout',inf); 
-sand0_side_left_xform = getTransform(tftree,'sandwich_0/base_link','sandwich_0/side_left_camera_link_optical','Timeout',inf); 
-sand0_rear_right_xform = getTransform(tftree,'sandwich_0/base_link','sandwich_0/rear_right_camera_link_optical','Timeout',inf); 
-sand0_rear_left_xform = getTransform(tftree,'sandwich_0/base_link','sandwich_0/rear_left_camera_link_optical','Timeout',inf);    
+       
 
 sand0_front_right = receive(sand0_front_right_sub,inf);
 sand0_front_left = receive(sand0_front_left_sub,inf);
@@ -114,47 +108,47 @@ send(sandwich_0_left_pub, sandwich_0_left_msg);
 send(sandwich_0_right_pub, sandwich_0_right_msg);
 
 % -------------------- Boat 1 -------------------------
-sand1_front_right_xform = getTransform(tftree,'sandwich_1/base_link','sandwich_1/front_right_camera_link_optical','Timeout',inf);  
-sand1_front_left_xform = getTransform(tftree,'sandwich_1/base_link','sandwich_1/front_left_camera_link_optical','Timeout',inf); 
-sand1_side_right_xform = getTransform(tftree,'sandwich_1/base_link','sandwich_1/side_right_camera_link_optical','Timeout',inf); 
-sand1_side_left_xform = getTransform(tftree,'sandwich_1/base_link','sandwich_1/side_left_camera_link_optical','Timeout',inf); 
-sand1_rear_right_xform = getTransform(tftree,'sandwich_1/base_link','sandwich_1/rear_right_camera_link_optical','Timeout',inf); 
-sand1_rear_left_xform = getTransform(tftree,'sandwich_1/base_link','sandwich_1/rear_left_camera_link_optical','Timeout',inf);    
-
-sand1_front_right = receive(sand1_front_right_sub,inf);
-sand1_front_left = receive(sand1_front_left_sub,inf);
-sand1_side_right = receive(sand1_side_right_sub,inf);
-sand1_side_left = receive(sand1_side_left_sub,inf);
-sand1_rear_right = receive(sand1_rear_right_sub,inf);
-sand1_rear_left = receive(sand1_rear_left_sub,inf);
-
-[sand1_ID_fr,sand1_pose_fr] = tag_detect(sand1_front_right);
-[sand1_ID_fl,sand1_pose_fl] = tag_detect(sand1_front_left);
-[sand1_ID_sr,sand1_pose_sr] = tag_detect(sand1_side_right);
-[sand1_ID_sl,sand1_pose_sl] = tag_detect(sand1_side_left);
-[sand1_ID_rr,sand1_pose_rr] = tag_detect(sand1_rear_right);
-[sand1_ID_rl,sand1_pose_rl] = tag_detect(sand1_rear_left);
-
-[sand1_tagID_fr,sand1_dist_fr,sand1_head_fr] = tag_disthead(sand1_ID_fr,sand1_pose_fr,sand1_front_right_xform);
-[sand1_tagID_fl,sand1_dist_fl,sand1_head_fl] = tag_disthead(sand1_ID_fl,sand1_pose_fl,sand1_front_left_xform);
-[sand1_tagID_sr,sand1_dist_sr,sand1_head_sr] = tag_disthead(sand1_ID_sr,sand1_pose_sr,sand1_side_right_xform);
-[sand1_tagID_sl,sand1_dist_sl,sand1_head_sl] = tag_disthead(sand1_ID_sl,sand1_pose_sl,sand1_side_left_xform);
-[sand1_tagID_rr,sand1_dist_rr,sand1_head_rr] = tag_disthead(sand1_ID_rr,sand1_pose_rr,sand1_rear_right_xform);
-[sand1_tagID_rl,sand1_dist_rl,sand1_head_rl] = tag_disthead(sand1_ID_rl,sand1_pose_rl,sand1_rear_left_xform);
-
-sand1_tagID = [sand1_tagID_fr, sand1_tagID_fl, sand1_tagID_sr, ...
-         sand1_tagID_sl, sand1_tagID_rr, sand1_tagID_rl];
-sand1_dist = [sand1_dist_fr, sand1_dist_fl, sand1_dist_sr,...
-        sand1_dist_sl, sand1_dist_rr, sand1_dist_rl];
-sand1_head = [sand1_head_fr, sand1_head_fl, sand1_head_sr,...
-        sand1_head_sl, sand1_head_rr, sand1_head_rl];
-
-[sand1_left, sand1_right] = aprilTag_boatfollow(sand1_tagID, sand1_dist, sand1_head)
-
-sandwich_1_left_msg.Data = sand1_left;
-sandwich_1_right_msg.Data = sand1_right;
-send(sandwich_1_left_pub, sandwich_1_left_msg);
-send(sandwich_1_right_pub, sandwich_1_right_msg);
+% sand1_front_right_xform = getTransform(tftree,'sandwich_1/base_link','sandwich_1/front_right_camera_link_optical','Timeout',inf);  
+% sand1_front_left_xform = getTransform(tftree,'sandwich_1/base_link','sandwich_1/front_left_camera_link_optical','Timeout',inf); 
+% sand1_side_right_xform = getTransform(tftree,'sandwich_1/base_link','sandwich_1/side_right_camera_link_optical','Timeout',inf); 
+% sand1_side_left_xform = getTransform(tftree,'sandwich_1/base_link','sandwich_1/side_left_camera_link_optical','Timeout',inf); 
+% sand1_rear_right_xform = getTransform(tftree,'sandwich_1/base_link','sandwich_1/rear_right_camera_link_optical','Timeout',inf); 
+% sand1_rear_left_xform = getTransform(tftree,'sandwich_1/base_link','sandwich_1/rear_left_camera_link_optical','Timeout',inf);    
+% 
+% sand1_front_right = receive(sand1_front_right_sub,inf);
+% sand1_front_left = receive(sand1_front_left_sub,inf);
+% sand1_side_right = receive(sand1_side_right_sub,inf);
+% sand1_side_left = receive(sand1_side_left_sub,inf);
+% sand1_rear_right = receive(sand1_rear_right_sub,inf);
+% sand1_rear_left = receive(sand1_rear_left_sub,inf);
+% 
+% [sand1_ID_fr,sand1_pose_fr] = tag_detect(sand1_front_right);
+% [sand1_ID_fl,sand1_pose_fl] = tag_detect(sand1_front_left);
+% [sand1_ID_sr,sand1_pose_sr] = tag_detect(sand1_side_right);
+% [sand1_ID_sl,sand1_pose_sl] = tag_detect(sand1_side_left);
+% [sand1_ID_rr,sand1_pose_rr] = tag_detect(sand1_rear_right);
+% [sand1_ID_rl,sand1_pose_rl] = tag_detect(sand1_rear_left);
+% 
+% [sand1_tagID_fr,sand1_dist_fr,sand1_head_fr] = tag_disthead(sand1_ID_fr,sand1_pose_fr,sand1_front_right_xform);
+% [sand1_tagID_fl,sand1_dist_fl,sand1_head_fl] = tag_disthead(sand1_ID_fl,sand1_pose_fl,sand1_front_left_xform);
+% [sand1_tagID_sr,sand1_dist_sr,sand1_head_sr] = tag_disthead(sand1_ID_sr,sand1_pose_sr,sand1_side_right_xform);
+% [sand1_tagID_sl,sand1_dist_sl,sand1_head_sl] = tag_disthead(sand1_ID_sl,sand1_pose_sl,sand1_side_left_xform);
+% [sand1_tagID_rr,sand1_dist_rr,sand1_head_rr] = tag_disthead(sand1_ID_rr,sand1_pose_rr,sand1_rear_right_xform);
+% [sand1_tagID_rl,sand1_dist_rl,sand1_head_rl] = tag_disthead(sand1_ID_rl,sand1_pose_rl,sand1_rear_left_xform);
+% 
+% sand1_tagID = [sand1_tagID_fr, sand1_tagID_fl, sand1_tagID_sr, ...
+%          sand1_tagID_sl, sand1_tagID_rr, sand1_tagID_rl];
+% sand1_dist = [sand1_dist_fr, sand1_dist_fl, sand1_dist_sr,...
+%         sand1_dist_sl, sand1_dist_rr, sand1_dist_rl];
+% sand1_head = [sand1_head_fr, sand1_head_fl, sand1_head_sr,...
+%         sand1_head_sl, sand1_head_rr, sand1_head_rl];
+% 
+% [sand1_left, sand1_right] = aprilTag_boatfollow(sand1_tagID, sand1_dist, sand1_head)
+% 
+% sandwich_1_left_msg.Data = sand1_left;
+% sandwich_1_right_msg.Data = sand1_right;
+% send(sandwich_1_left_pub, sandwich_1_left_msg);
+% send(sandwich_1_right_pub, sandwich_1_right_msg);
 
 % ------------------------ Boat 2 -------------------------
 % sand2_front_right_xform = getTransform(tftree,'sandwich_2/base_link','sandwich_2/front_right_camera_link_optical','Timeout',inf);  
