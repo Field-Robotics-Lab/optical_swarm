ae3a30facf9e598b3413b6544dfd26bf589a7d33
matthewheubach <mattheubach@live.com>
2022-08-02 10:50:13 -0700

    Add documentation to multifollow.m

--- matlab/multi_follow.m
+++ matlab/multi_follow.m
@@ -63,7 +63,7 @@ sandwich_2_right_msg = rosmessage(sandwich_2_right_pub);
 
 
 
-% -------------------- Transforms from camera to boat center
+% -------------------- Camera to Base Link Transforms ---------------------
 tftree = rostf;
 
 %---------------------- Boat 0 ----------------------------
@@ -91,11 +91,14 @@ sand2_side_left_xform = getTransform(tftree,'sandwich_2/base_link','sandwich_2/s
 sand2_rear_right_xform = getTransform(tftree,'sandwich_2/base_link','sandwich_2/rear_right_camera_link_optical','Timeout',inf); 
 sand2_rear_left_xform = getTransform(tftree,'sandwich_2/base_link','sandwich_2/rear_left_camera_link_optical','Timeout',inf);    
 
+
+
+
 while true
 
 %---------------------- Boat 0 ----------------------------
 
-
+%--------------Receive Tag Detection Message -------------
 sand0_front_right = receive(sand0_front_right_sub,inf);
 sand0_front_left = receive(sand0_front_left_sub,inf);
 sand0_side_right = receive(sand0_side_right_sub,inf);
@@ -103,6 +106,7 @@ sand0_side_left = receive(sand0_side_left_sub,inf);
 sand0_rear_right = receive(sand0_rear_right_sub,inf);
 sand0_rear_left = receive(sand0_rear_left_sub,inf);
 
+%------------- Extract  all ID and XYZ (Camera Frame) ------------
 [sand0_ID_fr,sand0_pose_fr] = tag_detect(sand0_front_right);
 [sand0_ID_fl,sand0_pose_fl] = tag_detect(sand0_front_left);
 [sand0_ID_sr,sand0_pose_sr] = tag_detect(sand0_side_right);
@@ -110,6 +114,7 @@ sand0_rear_left = receive(sand0_rear_left_sub,inf);
 [sand0_ID_rr,sand0_pose_rr] = tag_detect(sand0_rear_right);
 [sand0_ID_rl,sand0_pose_rl] = tag_detect(sand0_rear_left);
 
+%------------- Convert to Distance/Heading (Body Frame) ------------------
 [sand0_tagID_fr,sand0_dist_fr,sand0_head_fr] = tag_disthead(sand0_ID_fr,sand0_pose_fr,sand0_front_right_xform);
 [sand0_tagID_fl,sand0_dist_fl,sand0_head_fl] = tag_disthead(sand0_ID_fl,sand0_pose_fl,sand0_front_left_xform);
 [sand0_tagID_sr,sand0_dist_sr,sand0_head_sr] = tag_disthead(sand0_ID_sr,sand0_pose_sr,sand0_side_right_xform);
@@ -117,6 +122,7 @@ sand0_rear_left = receive(sand0_rear_left_sub,inf);
 [sand0_tagID_rr,sand0_dist_rr,sand0_head_rr] = tag_disthead(sand0_ID_rr,sand0_pose_rr,sand0_rear_right_xform);
 [sand0_tagID_rl,sand0_dist_rl,sand0_head_rl] = tag_disthead(sand0_ID_rl,sand0_pose_rl,sand0_rear_left_xform);
 
+%-------------- Consolidate Camera Outputs -----------------------
 sand0_tagID = [sand0_tagID_fr, sand0_tagID_fl, sand0_tagID_sr, ...
          sand0_tagID_sl, sand0_tagID_rr, sand0_tagID_rl];
 sand0_dist = [sand0_dist_fr, sand0_dist_fl, sand0_dist_sr,...
@@ -124,9 +130,10 @@ sand0_dist = [sand0_dist_fr, sand0_dist_fl, sand0_dist_sr,...
 sand0_head = [sand0_head_fr, sand0_head_fl, sand0_head_sr,...
         sand0_head_sl, sand0_head_rr, sand0_head_rl];
 
-% Range of AprilTag IDs
+% Specify Range of AprilTag IDs
 range0 = [90,91,92,93,94,95];
 
+%----------- Calculate and Publish Thrust Commands-----------------
 [debug0,sand0_left, sand0_right] = aprilTag_tagfollow(sand0_tagID, sand0_dist, sand0_head,range0)
 
 if isempty(debug0) == 0
@@ -142,6 +149,7 @@ else
 end
 
 % -------------------- Boat 1 -------------------------
+%--------------Receive Tag Detection Message -------------
 sand1_front_right = receive(sand1_front_right_sub,inf);
 sand1_front_left = receive(sand1_front_left_sub,inf);
 sand1_side_right = receive(sand1_side_right_sub,inf);
@@ -149,6 +157,7 @@ sand1_side_left = receive(sand1_side_left_sub,inf);
 sand1_rear_right = receive(sand1_rear_right_sub,inf);
 sand1_rear_left = receive(sand1_rear_left_sub,inf);
 
+%------------- Extract  all ID and XYZ (Camera Frame) ------------
 [sand1_ID_fr,sand1_pose_fr] = tag_detect(sand1_front_right);
 [sand1_ID_fl,sand1_pose_fl] = tag_detect(sand1_front_left);
 [sand1_ID_sr,sand1_pose_sr] = tag_detect(sand1_side_right);
@@ -156,6 +165,7 @@ sand1_rear_left = receive(sand1_rear_left_sub,inf);
 [sand1_ID_rr,sand1_pose_rr] = tag_detect(sand1_rear_right);
 [sand1_ID_rl,sand1_pose_rl] = tag_detect(sand1_rear_left);
 
+%------------- Convert to Distance/Heading (Body Frame) ------------------
 [sand1_tagID_fr,sand1_dist_fr,sand1_head_fr] = tag_disthead(sand1_ID_fr,sand1_pose_fr,sand1_front_right_xform);
 [sand1_tagID_fl,sand1_dist_fl,sand1_head_fl] = tag_disthead(sand1_ID_fl,sand1_pose_fl,sand1_front_left_xform);
 [sand1_tagID_sr,sand1_dist_sr,sand1_head_sr] = tag_disthead(sand1_ID_sr,sand1_pose_sr,sand1_side_right_xform);
@@ -163,6 +173,7 @@ sand1_rear_left = receive(sand1_rear_left_sub,inf);
 [sand1_tagID_rr,sand1_dist_rr,sand1_head_rr] = tag_disthead(sand1_ID_rr,sand1_pose_rr,sand1_rear_right_xform);
 [sand1_tagID_rl,sand1_dist_rl,sand1_head_rl] = tag_disthead(sand1_ID_rl,sand1_pose_rl,sand1_rear_left_xform);
 
+%-------------- Consolidate Camera Outputs -----------------------
 sand1_tagID = [sand1_tagID_fr, sand1_tagID_fl, sand1_tagID_sr, ...
          sand1_tagID_sl, sand1_tagID_rr, sand1_tagID_rl];
 sand1_dist = [sand1_dist_fr, sand1_dist_fl, sand1_dist_sr,...
@@ -170,9 +181,10 @@ sand1_dist = [sand1_dist_fr, sand1_dist_fl, sand1_dist_sr,...
 sand1_head = [sand1_head_fr, sand1_head_fl, sand1_head_sr,...
         sand1_head_sl, sand1_head_rr, sand1_head_rl];
 
-% Range of AprilTag IDs
+% Specify Range of AprilTag IDs
 range1 = [0,1,2,3];
 
+%----------- Calculate and Publish Thrust Commands-----------------
 [debug1,sand1_left, sand1_right] = aprilTag_boatfollow(sand1_tagID, sand1_dist, sand1_head, range1)
 
 if isempty(debug1) == 0
@@ -189,6 +201,7 @@ else
 end
 
 % ------------------------ Boat 2 -------------------------
+%--------------Receive Tag Detection Message -------------
 sand2_front_right = receive(sand2_front_right_sub,inf);
 sand2_front_left = receive(sand2_front_left_sub,inf);
 sand2_side_right = receive(sand2_side_right_sub,inf);
@@ -196,6 +209,7 @@ sand2_side_left = receive(sand2_side_left_sub,inf);
 sand2_rear_right = receive(sand2_rear_right_sub,inf);
 sand2_rear_left = receive(sand2_rear_left_sub,inf);
 
+%------------- Extract  all ID and XYZ (Camera Frame) ------------
 [sand2_ID_fr,sand2_pose_fr] = tag_detect(sand2_front_right);
 [sand2_ID_fl,sand2_pose_fl] = tag_detect(sand2_front_left);
 [sand2_ID_sr,sand2_pose_sr] = tag_detect(sand2_side_right);
@@ -203,8 +217,7 @@ sand2_rear_left = receive(sand2_rear_left_sub,inf);
 [sand2_ID_rr,sand2_pose_rr] = tag_detect(sand2_rear_right);
 [sand2_ID_rl,sand2_pose_rl] = tag_detect(sand2_rear_left);
 
-
-
+%------------- Convert to Distance/Heading (Body Frame) ------------------
 [sand2_tagID_fr,sand2_dist_fr,sand2_head_fr] = tag_disthead(sand2_ID_fr,sand2_pose_fr,sand2_front_right_xform);
 [sand2_tagID_fl,sand2_dist_fl,sand2_head_fl] = tag_disthead(sand2_ID_fl,sand2_pose_fl,sand2_front_left_xform);
 [sand2_tagID_sr,sand2_dist_sr,sand2_head_sr] = tag_disthead(sand2_ID_sr,sand2_pose_sr,sand2_side_right_xform);
@@ -212,6 +225,7 @@ sand2_rear_left = receive(sand2_rear_left_sub,inf);
 [sand2_tagID_rr,sand2_dist_rr,sand2_head_rr] = tag_disthead(sand2_ID_rr,sand2_pose_rr,sand2_rear_right_xform);
 [sand2_tagID_rl,sand2_dist_rl,sand2_head_rl] = tag_disthead(sand2_ID_rl,sand2_pose_rl,sand2_rear_left_xform);
 
+%-------------- Consolidate Camera Outputs -----------------------
 sand2_tagID = [sand2_tagID_fr, sand2_tagID_fl, sand2_tagID_sr, ...
          sand2_tagID_sl, sand2_tagID_rr, sand2_tagID_rl];
 sand2_dist = [sand2_dist_fr, sand2_dist_fl, sand2_dist_sr,...
@@ -220,9 +234,10 @@ sand2_head = [sand2_head_fr, sand2_head_fl, sand2_head_sr,...
         sand2_head_sl, sand2_head_rr, sand2_head_rl];
 
 
-% Range of AprilTag IDs
+% Specify Range of AprilTag IDs
 range2 = [10,11,12,13];
 
+%----------- Calculate and Publish Thrust Commands-----------------
 [debug2,sand2_left, sand2_right] = aprilTag_boatfollow(sand2_tagID, sand2_dist, sand2_head,range2)
 
 if isempty(debug2) == 0
